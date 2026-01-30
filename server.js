bbb40d138f0e6c5a70c14a1004d4daa82fd6cf7e
Sayed MD Safiuddin Safi <safiuddin102030@gmail.com>
2026-01-31 00:27:16 +0600

    request entity too large fixed

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
